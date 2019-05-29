7d9b1f61dd72c5f5fd24ed77f2bac8304faca0ab
Amr Elfiky <aelfiky@ucsd.edu>
2019-05-29 14:55:46 -0700

    cleaned up code, edited visualizations

--- ourCode/Visualization.py
+++ ourCode/Visualization.py
@@ -8,6 +8,8 @@ import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 from scipy import stats
 from sentimentAnalysis import sentiment_Analysis
+from afinn import Afinn
+
 """
 @authors:
     Amr Elfiky
@@ -15,21 +17,17 @@ from sentimentAnalysis import sentiment_Analysis
     Yuesong Shi
     Edward Zamora
 
+This is the visualization of the data we have gathered and analyzed.
 """
-"""
-This is the visualization of the data we get
-"""
-with open(r'C:\Users\zzxxlty\Desktop\ece143project-master\data\senateParty.txt', 'rb') as sp:
+
+with open(r'../data/senateParty.txt', 'rb') as sp:
     spDict = pickle.loads(sp.read())
-    # print(spDict)
 
-with open(r'C:\Users\zzxxlty\Desktop\ece143project-master\data\senateTweets.txt', 'rb') as st:
+with open(r'../data/senateTweets.txt', 'rb') as st:
     stDict = pickle.loads(st.read())
-    # print(stDict)
 
-with open(r'C:\Users\zzxxlty\Desktop\ece143project-master\data\senateTweetsClean.txt', 'rb') as stc:
+with open(r'../data/senateTweetsClean.txt', 'rb') as stc:
     stcDict = pickle.loads(stc.read())
-    # print(stcDict)
 
 stcDDict = {}  # This is the dict containing all the Democrat senators and their clean tweets
 stcRDict = {}  # Republic senators and their clean tweets
@@ -39,9 +37,8 @@ for key in stcDict:
     elif spDict[key] == 'R':
         stcRDict[key] = stcDict[key]
 
-with open(r'C:\Users\zzxxlty\Desktop\ece143project-master\data\senateSentiments.txt', 'rb') as ss:
+with open(r'../data/senateSentiments.txt', 'rb') as ss:
     ssDict = pickle.loads(ss.read())
-    # print(ssDict)
 
 sDDict = {}  # This is the dict containing all the Democrat senators and their sentiment scores
 sRDict = {}  # Republic senators adn their scores
@@ -51,9 +48,8 @@ for key in ssDict:
     elif spDict[key] == 'R':
         sRDict[key] = ssDict[key]
 
-# print(sDDict)
-# print(sRDict)
 
+plt.rcParams.update({'font.size': 14})
 plt.figure(1)  # bar graph of all sentiment score and two parties separately
 i = 0
 for key in ssDict:
@@ -78,7 +74,6 @@ for key in sDDict:
 plt.title('Sentiment score of the Democratic Party')
 plt.ylabel('score')
 plt.xlabel('senators')
-#plt.show()
 
 plt.figure(3)
 i = 0
@@ -89,15 +84,10 @@ for key in sRDict:
 plt.title('Sentiment score of Republican Party')
 plt.ylabel('score')
 plt.xlabel('senators')
-#plt.show()
-
-
 
 # (2)mean deviation,std,of two parties' score
-scoreD = []  # The score of Democratic party
-scoreR = []  # The score of Republic party
-scoreD = [v for v in sDDict.values()]
-scoreR = [v for v in sRDict.values()]
+scoreD = [v for v in sDDict.values()]  # The score of Democratic party
+scoreR = [v for v in sDDict.values()]  # The score of Republic party
 print(scoreD)
 print(scoreR)
 
@@ -107,50 +97,23 @@ col_labels = ['Democratic Party', 'Republican Party']
 row_labels = ['max', 'min', 'mean', 'median', 'mode', 'variance', 'std deviation']
 col_colors = ['blue', 'red']
 
-max = []
-max.append(np.amax(D))
-max.append(np.amax(R))
-print(max)
+# get max, min, mean, median, mode, std for democrat and republican results
+max = [np.amax(D), np.amax(R)]
+min = [np.amin(D), np.amin(R)]
+mean = [np.mean(D), np.mean(R)]
+median = [np.median(D), np.median(R)]
+mode = [stats.mode(D)[0][0], stats.mode(R)[0][0]]
+variance = [np.var(D), np.var(R)]
+std = [np.std(D), np.std(R)]
 
-min = []
-min.append(np.amin(D))
-min.append(np.amin(R))
-print(min)
-
-mean = []
-mean.append(np.mean(D))
-mean.append(np.mean(R))
-print(mean)
-
-median = []
-median.append(np.median(D))
-median.append(np.median(R))
-print(median)
-
-mode = []
-mode.append(stats.mode(D)[0][0])
-mode.append(stats.mode(R)[0][0])
-print(mode)
-
-variance = []
-variance.append(np.var(D))
-variance.append(np.var(R))
-print(variance)
-
-std = []
-std.append(np.std(D))
-std.append(np.std(R))
-print(std)
-
-table_vals = [max, min, mean, median, mode, variance, std]
+table_vals = np.around([max, min, mean, median, mode, variance, std], decimals=3)
 print(table_vals)
 
 plt.figure(4)
-plt.table(cellText=table_vals, cellLoc='center',rowLabels=row_labels, colLabels=col_labels, colColours=col_colors,loc='best')
+plt.table(cellText=table_vals, cellLoc='center',rowLabels=row_labels, colLabels=col_labels,
+          colColours=col_colors,colWidths=[0.4 for x in range(len(table_vals))], loc='best')
 plt.title('Statistics')
 plt.axis('off')
-#plt.show()
-
 
 # (3)box graph for two party
 labels = ['Democratic','Republican']
@@ -158,17 +121,16 @@ color = dict(boxes = "DarkGreen", whiskers = "DarkOrange", medians = "DarkBlue",
 plt.figure(5)
 plt.boxplot([D,R],labels=labels,sym='o')
 plt.title('Box plot')
-#plt.show()
 
-# stcDDict
-# stcRDict
 # create two txt files that only contains the clean tweets of two parties.
+afinn = Afinn()
 D = open('stcD.txt', 'w+',encoding='utf-8')
 for key in stcDDict:
     for item in stcDDict[key]:
         for element in item:
-            D.write(element)
-            D.write('\n')
+            if afinn.score(element) != 0:
+                D.write(element)
+                D.write('\n')
 
 
 D.close()
@@ -177,26 +139,25 @@ R = open('stcR.txt', 'w+',encoding='utf-8')
 for key in stcRDict:
     for item in stcRDict[key]:
         for element in item:
-            R.write(element)
-            R.write('\n')
-
+            if afinn.score(element) != 0:
+                R.write(element)
+                R.write('\n')
 
 D.close()
 
 
-# word cloud for two parties
+#word cloud for two parties
 plt.figure(6)
-textD = open(r'C:\Users\zzxxlty\Desktop\ece143project-master\ourCode\stcD.txt','r').read()
+textD = open(r'./stcD.txt','r').read()
 wcD = WordCloud(background_color='white', scale=1.5).generate(textD)
 plt.imshow(wcD)
-plt.title('Word cloud of Democratic Party')
+#plt.title('Word cloud of Democratic Party')
 plt.axis('off')
-#plt.show()
 
 plt.figure(7)
-textR = open(r'C:\Users\zzxxlty\Desktop\ece143project-master\ourCode\stcR.txt','r').read()
+textR = open(r'./stcR.txt','r').read()
 wcR = WordCloud(background_color='white', scale=1.5).generate(textR)
 plt.imshow(wcR)
-plt.title('Word cloud of Republican Party')
+#plt.title('Word cloud of Republican Party')
 plt.axis('off')
 plt.show()
